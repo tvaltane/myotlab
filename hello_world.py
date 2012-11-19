1519590d1305d08547b7acf514f849fbc03ec049
Joonas Nikula <jonikula@paju.oulu.fi>
2012-11-19 12:38:03 +0200

    edited

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,11 @@
+import time
 def main():
-	name = raw_input("What's your name?")
+	name = raw_input("What's your name? ")
 	print "hello " + name
-
+	print "This is so awesome!"
+	print "lollollol"
+	input2 = int(raw_input("Gimme some numbers! "))
+	input3 = int(raw_input("Gimme another one! "))
+	print input2 + input3
 main()
 	
